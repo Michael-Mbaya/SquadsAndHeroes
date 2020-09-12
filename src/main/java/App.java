0c40b00e6b63cb803ca23ced8ad295a179c3caf3
Michael-Mbaya <gikundamike@gmail.com>
2020-09-12 16:36:02 +0300

    Squad Object properties,getter  and clearAll method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,6 +13,14 @@ public class App {
 
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
 
 
     }
--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -3,8 +3,45 @@ import java.util.List;
 
 public class Squad {
 
-    public static void main(String[] args) {
+    private int maxSize;
+    private String name;
+    private String cause;
+    private static List<Squad> instances = new ArrayList<Squad>();
+    private int id;
 
+        //constructor for Squad
+    public Squad(int maxSize, String squadName, String squadCause) {
+        this.maxSize = maxSize;
+        this.name = squadName;
+        this.cause = squadCause;
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    //getter for a Squad size(members are Hero's)
+    public int getMaxSize() {
+        return maxSize;
+    }
+    //getter for a Squad name
+    public String getName() {
+        return name;
+    }
+    //getter for a Squad cause
+    public String getCause() {
+        return cause;
+    }
+    //getter for a Squad id
+    public int getId() {
+        return id;
+    }
+    //getter/return list of Squad Instances
+    public static List<Squad> all() {
+        return instances;
+    }
+    //clear all Squad instances
+    public static void clearAll() {
+        instances.clear();
     }
 
 }
+
